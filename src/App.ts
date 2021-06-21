a49cfa745a62ed3ae5a675f751c40c90862efdb3
chicichich <pch6789@naver.com>
2021-06-21 23:17:11 +0900

    Add: 시작 멈춤 버튼

--- src/App.ts
+++ src/App.ts
@@ -6,7 +6,8 @@ class App {
   $app: HTMLDivElement
   $wheelSide: HTMLDivElement
   $boardSide: HTMLDivElement
-  constructor($target: HTMLElement){
+  $rollButton: HTMLButtonElement
+  constructor($target: HTMLElement, wheelName: string){
     this.$target = $target;
     this.$app = document.createElement('div');
     this.$app.className = "random-wheel"
@@ -18,9 +19,23 @@ class App {
     this.$app.appendChild(this.$wheelSide)
     this.$app.appendChild(this.$boardSide)
     const title:HTMLHeadingElement = document.createElement('h1');
+    title.textContent = wheelName;
     this.$wheelSide.appendChild(title);
-    new Wheel(this.$wheelSide)
+    const wheel = new Wheel(this.$wheelSide)
+    this.$rollButton = document.createElement('button')
+    this.$rollButton.textContent = "시작"
+    this.$rollButton.onclick = () => {
+      if(wheel.isRun) {
+        wheel.stop()
+        this.$rollButton.textContent = "시작"
+      } else {
+        wheel.run();
+        this.$rollButton.textContent = "멈춤"
+      }
+    }
+    this.$wheelSide.appendChild(this.$rollButton);
   }
+
 }
 
 export default App;
