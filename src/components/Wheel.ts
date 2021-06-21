526b8b3619ce9fdaae40659f80fe12db203a6b99
chicichich <pch6789@naver.com>
2021-06-21 23:01:08 +0900

    Add: roll method

--- src/components/Wheel.ts
+++ src/components/Wheel.ts
@@ -4,6 +4,8 @@ class Wheel {
   $target: HTMLElement
   $canvas: HTMLCanvasElement
   $ctx: CanvasRenderingContext2D
+  proposition:proposition[]
+  rotateAngle: number;
   constructor($target: HTMLElement){
     this.$target = $target;
     this.$canvas = document.createElement('canvas')
@@ -11,12 +13,15 @@ class Wheel {
     this.$canvas.height = 700;
     this.$ctx = (this.$canvas.getContext('2d') as CanvasRenderingContext2D)
     this.$target.appendChild(this.$canvas);
+    this.proposition =  [{label: '돌림판', rate: 33.333}, {label: '돌림판', rate: 33.333}, {label: '돌림판', rate: 33.333}]
+    this.rotateAngle = 0;
     this.render();
+    this.roll(400);
   }
-  render(proposition:proposition[] = [{label: '돌림판', rate: 33.333}, {label: '돌림판', rate: 33.333}, {label: '돌림판', rate: 33.333}]){
+  render(){
     const ctx = this.$ctx;
-    let accAngle = 0;
-    proposition.forEach(({label, rate}, index) => {
+    let accAngle = this.rotateAngle;
+    this.proposition.forEach(({label, rate}, index) => {
       const beginRadian = accAngle / 180 * Math.PI;
       accAngle += rate * 3.6;
       const endRadian = accAngle / 180 * Math.PI;
@@ -28,6 +33,14 @@ class Wheel {
       ctx.arc(350, 350, 300, beginRadian - (Math.PI * 0.5), endRadian - (Math.PI * 0.5));
       ctx.stroke();
     })
+  }
+  roll(targetAngle: number, speed = 10){
+    this.$ctx.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
+    this.render()
+    if(this.rotateAngle <= targetAngle){
+      this.rotateAngle += speed;
+      requestAnimationFrame(this.roll.bind(this, targetAngle, speed))
+    }
   }
 }
 
